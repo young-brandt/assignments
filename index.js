12078eb0ded1bc4b3eee8cdc3427e6bf3957e34d
Brandt Young <young.brandt66@gmail.com>
2022-08-21 12:49:10 -0600

    Made Style Changes via JavaScript

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ header.textContent = 'JavaScript Made This'
 var newHeader = document.getElementById('body')
 newHeader.prepend(header)
 
+headerTwo.style.textAlign = 'center';
+headerTwo.style.fontWeight = '1000';
+
+function myFunction() {
+    document.getElementById("clear-button").reset();
+  }
+
+var myText = document.getElementById('message one')
+myText.textContent = 'Hello, how are you?';
+
+var myText = document.getElementById('message two')
+myText.textContent = 'I am good, how are you?';
+
+var myText = document.getElementById('message three')
+myText.textContent = 'That is great, how is the weather?';
+
+var myText = document.getElementById('message four')
+myText.textContent = 'It is great as well.';
